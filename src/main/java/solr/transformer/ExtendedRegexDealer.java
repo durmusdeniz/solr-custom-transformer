2e1fd2fe83644bfd00b76a01cbf8d6a2e4723c55
Deniz Durmus <denizdurmus87@gmail.com>
2019-03-04 20:30:19 +0800

    initial commit

---
+++ src/main/java/solr/transformer/ExtendedRegexDealer.java
@@ -0,0 +1,16 @@
+package solr.transformer;
+
+import org.apache.solr.handler.dataimport.Context;
+import org.apache.solr.handler.dataimport.Transformer;
+
+import java.util.Map;
+
+
+public class ExtendedRegexDealer extends Transformer
+{
+
+    @Override
+    public Object transformRow(Map<String, Object> map, Context context) {
+        return null;
+    }
+}
